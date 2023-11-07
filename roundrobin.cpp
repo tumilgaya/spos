c5727c93cc0e4a95cb05e755680d67edc2e71755
tumilgaya <149937502+tumilgaya@users.noreply.github.com>
2023-11-07 23:40:51 +0530

    Update roundrobin.cpp

--- roundrobin.cpp
+++ roundrobin.cpp
@@ -34,33 +34,33 @@ int main()
         cout << "Enter the Burst Time for " << arr[i].pro_name << ": ";
         cin >> arr[i].burst_time;
 
-        arr[i].remaining_burst_time = arr[i].burst_time;
+        arr[i].remaining_burst_time = arr[i].burst_time;    //starting will always have remaining time
     }
 
     int current_time = 0;
-    bool all_processes_completed = false;
+    int all_processes_completed = 0;
 
     while (!all_processes_completed)
     {
-        all_processes_completed = true; // assume
+        all_processes_completed = 1; // assume all process have finished
 
-        for (int i = 0; i < no_procs; i++)
+        for (int i = 0; i < no_procs; i++)    //check for every process
         {
-            if (arr[i].remaining_burst_time > 0 && arr[i].arrival_time <= current_time)
+            if (arr[i].remaining_burst_time > 0 && arr[i].arrival_time <= current_time)    //if there is some remaining time and the process is about to arrive by compaing current time
             {
-                all_processes_completed = false;
+                all_processes_completed = 0;    //this implies all process is not completed
 
-                if (arr[i].remaining_burst_time <= time_quantum)
+                if (arr[i].remaining_burst_time <= time_quantum)    //if it's about to finish and it's less than quantum time
                 {
-                    current_time = current_time + arr[i].remaining_burst_time;
-                    arr[i].turn_around_time = current_time - arr[i].arrival_time;
-                    arr[i].waiting_time = arr[i].turn_around_time - arr[i].burst_time;
-                    arr[i].remaining_burst_time = 0;
+                    current_time = current_time + arr[i].remaining_burst_time;    //update current time by adding remainingBurstTime
+                    arr[i].turn_around_time = current_time - arr[i].arrival_time;    //tat = currTime - arrTime
+                    arr[i].waiting_time = arr[i].turn_around_time - arr[i].burst_time;    //wt = tat - bt
+                    arr[i].remaining_burst_time = 0;    //now implies remaining burst time is done
                 }
-                else
+                else    //otherwise process is not near process time
                 {
-                    current_time = current_time + time_quantum;
-                    arr[i].remaining_burst_time = arr[i].remaining_burst_time - time_quantum;
+                    current_time = current_time + time_quantum;    //update currenttime by currTime + timeQuantum
+                    arr[i].remaining_burst_time = arr[i].remaining_burst_time - time_quantum;    //update remaining time as quantum time
                 }
             }
         }
